091559b23a2f8fedc5595c9699839c83b8caa0c5
InnaMcGrane <innamcgrane@gmail.com>
2024-09-26 08:14:45 +0100

    create data

--- script.js
+++ script.js
@@ -49,5 +49,68 @@ class Stone {
   }
 }
 
+const data = [
+  {
+    id: 346,
+    color: "blue",
+    img: "t2",
+  },
+  {
+    id: 2345,
+    color: "red",
+    img: "t1",
+  },
+  {
+    id: 7,
+    color: "green",
+    img: "t3",
+  },
+  {
+    id: 235,
+    color: "violet",
+    img: "t4",
+  },
+  {
+    id: 567,
+    color: "blue",
+    img: "t2",
+  },
+  {
+    id: 67,
+    color: "red",
+    img: "t1",
+  },
+  {
+    id: 43,
+    color: "green",
+    img: "t3",
+  },
+  {
+    id: 87,
+    color: "violet",
+    img: "t4",
+  },
+  {
+    id: 95,
+    color: "blue",
+    img: "t2",
+  },
+  {
+    id: 352,
+    color: "red",
+    img: "t1",
+  },
+  {
+    id: 3456,
+    color: "green",
+    img: "t3",
+  },
+  {
+    id: 323446,
+    color: "violet",
+    img: "t4",
+  },
+];
+
 const root = document.querySelector(".root");
 root.insertAdjacentElement("beforeend", new Game().element);
